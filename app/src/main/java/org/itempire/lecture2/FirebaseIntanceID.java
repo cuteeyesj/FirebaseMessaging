c769ff0bff346aedda119120a000a037dfea7682
kamranismail <cuteeyesj@gmail.com>
2018-08-15 10:49:18 +0500

    Main gradle added.

--- app/src/main/java/org/itempire/lecture2/FirebaseIntanceID.java
+++ app/src/main/java/org/itempire/lecture2/FirebaseIntanceID.java
@@ -13,6 +13,7 @@ public class FirebaseIntanceID extends FirebaseInstanceIdService {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d("FROM FIREBASE", "Refreshed token: " + refreshedToken);
 
+
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
